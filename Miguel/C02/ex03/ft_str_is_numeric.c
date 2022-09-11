aa5409955a0f0f3d283e35495a7d3460e17cc4fc
polltter <miguelr.silva@outlook.pt>
2022-09-11 19:31:52 +0100

    exercise 03

---
+++ Miguel/C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,35 @@
+int	str_len(char *str);
+int	is_char_num(char c);
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str_len(str))
+		return (1);
+	while (*(str + i) != '\0')
+	{
+		if (!is_char_num(*(str + i)))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	is_char_num(char c)
+{
+	if ((c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+int	str_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+		i++;
+	return (i);
+}
---
+++ Miguel/C02/ex03/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str);
+
+int	main(void)
+{
+	char	*str = "";
+	printf("%d\n", ft_str_is_numeric(str));
+	return (0);
+}
