a6d35e98fdbffb98288611baa2351bd879dd1162
Manuel Tiago Simoes Teodoro <mtiago-s@c1r17s6.42lisboa.com>
2022-09-20 17:17:18 +0100

    ex03 c07 correct

--- Tiago/c07/ex03/ft_strjoin.c
+++ Tiago/c07/ex03/ft_strjoin.c
@@ -6,11 +6,36 @@
 /*   By: mtiago-s <mtiago-s@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 10:17:41 by mtiago-s          #+#    #+#             */
-/*   Updated: 2022/09/19 14:52:37 by mtiago-s         ###   ########.fr       */
+/*   Updated: 2022/09/20 17:09:35 by mtiago-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+int	ft_omegacount(char **strs, char *sep, int size)
+{
+	int	s;
+	int	i;
+	int	n;
+
+	s = 0;
+	i = 0;
+	while (i < size)
+	{
+		n = 0;
+		while (strs[i][n])
+		{
+			s++;
+			n++;
+		}
+		i++;
+	}
+	i = 0;
+	while (sep[i])
+		i++;
+	s += i * (size - 1);
+	return (s);
+}
 
 char	*ft_strcat(char *dest, char *src)
 {
@@ -30,20 +55,20 @@ char	*ft_strcat(char *dest, char *src)
 	return (dest);
 }
 
-char	*ft_strjoin_assist(int size, char **strs, char *sep)
+char	*ft_strjoin_assist(int size, char **strs, char *sep, char *str)
 {
-	int	a;
-	int	k;
-	int	i;
+	int		a;
+	int		k;
+	int		i;
 
-	k = 1;
+	k = 0;
 	i = 0;
-	a = 0;
-	while (size > 0 || strs[a] != '\0')
+	a = 1;
+	while (size > 1 && strs[a])
 	{
 		if (k == 1 && strs[a])
 		{
-			ft_strcat(str, str[a]);
+			ft_strcat(str, strs[a]);
 			a++;
 			k = 0;
 			size--;
@@ -60,10 +85,22 @@ char	*ft_strjoin_assist(int size, char **strs, char *sep)
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*str;
+	int		i;
 
-	str = (char *)malloc(sizeof (char) * size);
-	if (size == 0)
-		return ((char *)malloc(sizeof(char)));
-	str = ft_strjoin_assist(size, strs, sep);
-	return (str);
+	i = 0;
+	if (size >= 0)
+	{
+		str = (char *)malloc(sizeof (char) * ft_omegacount(strs, sep, size) + 1);
+		if (size == 0)
+			return ((char *)malloc(sizeof(char)));
+		while (strs[0][i] != '\0')
+		{
+			str[i] = strs[0][i];
+			i++;
+		}
+		str[i] = '\0';
+		str = ft_strjoin_assist(size, strs, sep, str);
+		return (str);
+	}
+	return (0);
 }
