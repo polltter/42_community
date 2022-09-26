475b4fa535bc0d6a2db3a573032d26a07428d7ea
Manuel Tiago Simoes Teodoro <mtiago-s@c1r7s6.42lisboa.com>
2022-09-26 16:25:05 +0100

    map_check

--- BSQ/SRCS/map_check.c
+++ BSQ/SRCS/map_check.c
@@ -6,14 +6,59 @@
 /*   By: mtiago-s <mtiago-s@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 15:05:16 by mtiago-s          #+#    #+#             */
-/*   Updated: 2022/09/26 15:25:23 by mtiago-s         ###   ########.fr       */
+/*   Updated: 2022/09/26 16:06:11 by mtiago-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_check_map(char *mapf, )
-{
-	int	i;
-	int	j;
+#include "bsq.h"
 
-	return (0);
+int	ft_check_map_2(int f, char c, t_file mapS)
+{
+	int	o;
+	int	i;
+
+	o = 0;
+	while (c != EOF)
+	{
+		i = 1;
+		read(f, &c, 1);
+		while (c != '\n' && c != EOF)
+		{
+			if (c == mapS.obs)
+				o++;
+			else if (c != mapS.empty)
+				return (0);
+			read(f, &c, 1);
+			i++;
+		}
+		if (i != mapS.col + 1)
+			return(0);
+	}
+	if (o = 0)
+		return (0);
+	return (1);
+}
+
+int	ft_check_map(char *map, t_file mapS)
+{
+	int		f;
+	char	c;
+	int		i;
+
+	f = open(map, 0);
+	read(f, &c, 1);
+	i = 0;
+	while(c != '\n')
+	{
+		read(f, &c, 1);
+		i++;
+	}
+	if (i < 4 || mapS.empty == mapS.obs || mapS.empty == mapS.full 
+			|| mapS.obs == mapS.full || mapS.empty < 32 
+			|| mapS.empty > 127 || mapS.obs < 32 || mapS.obs > 127 
+			|| mapS.full < 32 || mapS.full > 127)
+		return (0);
+	if (!(ft_check_map_2(f, c, mapS)))
+		return (0);
+	return (1);
 }
