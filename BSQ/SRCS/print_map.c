5288701889daf70fb97f9d8a5730b1333c2b88b5
Manuel Tiago Simoes Teodoro <mtiago-s@c1r7s6.42lisboa.com>
2022-09-27 18:54:46 +0100

    print map added

---
+++ BSQ/SRCS/print_map.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtiago-s <mtiago-s@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 16:26:22 by mtiago-s          #+#    #+#             */
+/*   Updated: 2022/09/27 17:00:56 by mtiago-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bsq.h"
+
+void	ft_putstr(char *str)
+{
+	while(*str)
+	{
+		write(1, &*str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_print_map(char **matrix, t_file mapf)
+{
+	int	i;
+
+	i = 0;
+	while (i < mapf.lines)
+	{
+		ft_putstr(matrix[i]);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_put_sq(char **matrix, t_file mapf, int size, t_coord first_coord)
+{
+	int	i;
+	int	j;
+	int	n;
+	int	k;
+
+	i = first_coord.x;
+	k = size;
+	while (k > 0)
+	{
+		n = size;
+		j = first_coord.y;
+		while (n > 0)
+		{
+			matrix[i][j] = mapf.full;
+			j++;
+			n--;
+		}
+		i++;
+		k--;
+	}
+	ft_print_map(matrix, mapf);
+}
