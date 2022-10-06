4806b717c2b45e47354f77c49a60eb7f598bde48
Manuel Teodoro <manuelteodoro@MacBook-Pro-de-Manuel-3.local>
2022-10-06 12:50:43 +0100

    c09 added to tiago

---
+++ Tiago/C09/ex02/ft_split.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtiago-s <mtiago-s@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 13:04:58 by mtiago-s          #+#    #+#             */
+/*   Updated: 2022/09/22 14:20:37 by mtiago-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	check_sep(char c, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	count_strings(char *str, char *charset)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] != '\0' && check_sep(str[i], charset))
+			i++;
+		if (str[i] != '\0')
+			count++;
+		while (str[i] != '\0' && !check_sep(str[i], charset))
+			i++;
+	}
+	return (count);
+}
+
+int	ft_strlen_sep(char *str, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0' && !check_sep(str[i], charset))
+		i++;
+	return (i);
+}
+
+char	*ft_word(char *str, char *charset)
+{
+	int		len_word;
+	int		i;
+	char	*word;
+
+	i = 0;
+	len_word = ft_strlen_sep(str, charset);
+	word = (char *)malloc(sizeof(char) * (len_word + 1));
+	while (i < len_word)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**res;
+	int		i;
+
+	i = 0;
+	res = (char **)malloc(sizeof(char *)
+			* (count_strings(str, charset) + 1));
+	while (*str != '\0')
+	{
+		while (*str != '\0' && check_sep(*str, charset))
+			str++;
+		if (*str != '\0')
+		{
+			res[i] = ft_word(str, charset);
+			i++;
+		}
+		while (*str && !check_sep(*str, charset))
+			str++;
+	}
+	res[i] = 0;
+	return (res);
+}
